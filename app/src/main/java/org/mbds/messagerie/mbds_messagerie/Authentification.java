26fa0eeb7d6aff769b823773f44d84eecb516293
Ines NASR <ines.nasr.93@gmail.com>
2018-11-11 19:53:52 +0100

    Hello World

---
+++ app/src/main/java/org/mbds/messagerie/mbds_messagerie/Authentification.java
@@ -0,0 +1,13 @@
+package org.mbds.messagerie.mbds_messagerie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Authentification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentification);
+    }
+}
